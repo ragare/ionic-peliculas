b85e8368a9e9f2cd279f2ed8ae90d8d01b6fa1eb
Rafael Garcia <dev.ragare@gmail.com>
2019-12-22 17:18:06 +0100

    Pantalla de detalles

--- src/app/components/detalle/detalle.component.ts
+++ src/app/components/detalle/detalle.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
+import { PeliculaDetalle, Cast } from '../../interfaces/interfaces';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-detalle',
@@ -10,16 +12,40 @@ export class DetalleComponent implements OnInit {
 
   @Input() id;
 
-  constructor(private moviesService: MoviesService) { }
+  pelicula: PeliculaDetalle;
+  numCaracteres = 150;
+  actores: Cast[] = [];
+
+  slideOptActores = {
+    slidesPerView: 3.3,
+    freeMode: true,
+    spacebetween: -5
+  };
+
+  constructor(private moviesService: MoviesService, private modalCtrl: ModalController) { }
 
   ngOnInit() {
     this.moviesService.getPeliculaDetalle(this.id).subscribe(resp => {
       console.log('Detalle', resp);
+      this.pelicula = resp;
     });
 
     this.moviesService.getActoresPelicula(this.id).subscribe(resp => {
       console.log('Credits', resp);
+      this.actores = resp.cast;
     });
+
+  }
+
+  mostrarOverviewCompleto() {
+    this.numCaracteres = this.pelicula.overview.length;
+  }
+
+  regresar() {
+    this.modalCtrl.dismiss();
+  }
+
+  favorito() {
 
   }
 
